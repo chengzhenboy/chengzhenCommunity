6ddfe956bfe77dca12df6fd2a2de35b29fcb91ad
wuchengzhen <wuchengzhen@hzzl.net>
2020-03-15 17:14:17 +0800

    Update IndexController.java
    上传bootstrap框架
    
    Co-Authored-By: chengzhen <chengzhen@users.noreply.github.com>

--- src/main/java/com/life/communtity/controller/IndexController.java
+++ src/main/java/com/life/communtity/controller/IndexController.java
@@ -11,10 +11,9 @@ import org.springframework.web.bind.annotation.RequestParam;
  * @ver:1.0
  **/
 @Controller
-public class HelloController {
-    @GetMapping("hello")
-    public String Hello(@RequestParam(name="name")String name, Model model){
-            model.addAttribute("name",name);
-        return "hello";
+public class IndexController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
     }
 }
